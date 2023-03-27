5a3a9106a86f0b18ef2c3ae375d9c25e01915130
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-03-27 09:13:09 +0530

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,21 @@
 const sumAll = function(num1,num2) {
+    let l = 0;
+    let h = 0;
+    if (num1<0 || num2<0 || typeof num1 != 'number' || typeof num2 != 'number') {
+        return "ERROR";
+    }
+    if (num1 < num2) {
+        l = num1;
+        h = num2;
+    }
+    else{
+        l = num2;
+        h = num1;
+    }
     let res = 0;
-    for(let i = num1; i <= num2; i++) {
-        res += i;
+    while (l<=h){
+        res += l;
+        l += 1;
     }
     return res;
 };
