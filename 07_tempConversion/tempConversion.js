b9496fb5e39ad4d060798674b99b4c6f5b35b6aa
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-03-27 10:50:14 +0530

    exercise 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let cels = 0;
+  cels = Math.round((((temp-32)*5)/9)*10)/10;
+  return cels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahr = 0;
+  fahr = Math.round((((temp/5)*9)+32)*10)/10;
+  return fahr;
 };
 
 // Do not edit below this line
