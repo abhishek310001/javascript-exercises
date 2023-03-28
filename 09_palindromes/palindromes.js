c7fc8e59e6f7d946ecd374224c0fdab5c9c993aa
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-03-28 13:26:47 +0530

    exercise 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let revStr = '';
+    let orgStr = '';
+    for(let i = str.length - 1; i >= 0; i--) {
+        if ((str.charCodeAt(i)>=65 && str.charCodeAt(i)<=90) || 
+        (str.charCodeAt(i)>=97 && str.charCodeAt(i)<=122)){
+            revStr += str[i];
+        }
+    }
+    for(let i = 0;i < str.length;i++){
+        if ((str.charCodeAt(i)>=65 && str.charCodeAt(i)<=90) || 
+        (str.charCodeAt(i)>=97 && str.charCodeAt(i)<=122)){
+            orgStr += str[i];
+        }
+    }
+    return (revStr==orgStr);
 };
 
 // Do not edit below this line
