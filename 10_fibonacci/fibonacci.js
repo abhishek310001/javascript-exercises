d527f1f5d9353fdb23abd6c0b82986c82c8fa6a4
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-03-28 13:34:36 +0530

    exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0){
+        return "OOPS"
+    }
+    let a = 1,b = 1;
+    let s = 0;
+    if (n==1 || n==2){
+        return 1;
+    }
+    for (let i = 3; i <= n; i++){
+        s = a + b;
+        a = b;
+        b = s;
+    }
+    return s;
 };
 
 // Do not edit below this line
