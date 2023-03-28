3639f5da84c7751e4bc97c0df17d1d340e552b67
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-03-28 14:11:20 +0530

    exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
 const findTheOldest = function(arr) {
-
+    let max = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].yearOfDeath == undefined){
+            age = 2023 - arr[i].yearOfBirth
+        }
+        else {
+            age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+        }
+        if (age > max){
+            max = age;
+            ind = i;
+        }
+    }
+    return arr[ind];
 };
 
 // Do not edit below this line
