822c534fed9a559d5566154a33e12fa98a2199db
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-03-28 13:04:34 +0530

    exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  for ( i = 0; i < arr.length; i++) {
+    prod *= arr[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	prod  = 1;
+  for (let i = 0; i < power; i++) {
+    prod *= num;
+  }
+  return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
