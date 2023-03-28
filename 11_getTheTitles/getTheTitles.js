6bfc0d0418abdc672c45d51feb48022c02e56d38
Abhishek Verma <128154648+abhishek310001@users.noreply.github.com>
2023-03-28 13:57:35 +0530

    exercise 11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++){
+        res.push(arr[i].title)
+    }
+    return res;
 };
 
 // Do not edit below this line
